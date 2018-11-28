038ff1e52a2abbbecf370d810882a39a086c0559
zōng yǔ <gxcsoccer@users.noreply.github.com>
2018-11-28 11:12:34 +0800

    fix: should auto recovery from zk disconnected or expired (#27)
    close https://github.com/alipay/sofa-rpc-node/issues/25

---
+++ test/registry/edge_case.test.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const mm = require('mm');
+const assert = require('assert');
+const sleep = require('mz-modules/sleep');
+const ZookeeperClient = require('zookeeper-cluster-client/lib/api_client');
+const ZookeeperRegistry = require('../../lib/registry/zk/data_client');
+
+const cluster = function(clazz, options = {}) {
+  options.port = 7778;
+  return require('cluster-client')(clazz, options);
+};
+const zookeeper = {};
+zookeeper.createClient = (connectionString, options) => {
+  options = options || {};
+  options.connectionString = connectionString || 'localhost:2181';
+  options.cluster = cluster;
+  return new ZookeeperClient(options);
+};
+const logger = console;
+const innerClient = Symbol.for('ClusterClient#innerClient');
+
+describe('test/registry/edge_case.test.js', () => {
+  afterEach(mm.restore);
+
+  it('should recover from disconnected', async function() {
+    this.timeout(60000);
+    const registry = new ZookeeperRegistry({
+      logger,
+      zookeeper,
+      address: '127.0.0.1:2181',
+    });
+    await registry.ready();
+
+    const interfaceName = 'com.nodejs.test.registry.TestService';
+
+    await registry.register({
+      interfaceName,
+      url: 'bolt://127.0.0.2:12200',
+    });
+
+    let firstVal;
+    registry.subscribe({
+      interfaceName,
+    }, val => {
+      registry.emit('first', val);
+      console.log('first', val);
+      firstVal = val;
+    });
+
+    let val = await registry.await('first');
+    assert.deepEqual(val, [ 'bolt://127.0.0.2:12200' ]);
+
+    const connectionManager = registry._zkClient._client[innerClient]._realClient._zookeeperClient.connectionManager;
+    const connect = connectionManager.connect;
+
+    mm(connectionManager, 'connect', function() {
+      setTimeout(() => {
+        mm.restore();
+        connect.call(connectionManager);
+      }, 40000);
+    });
+    connectionManager.socket.destroy();
+
+    const registry2 = new ZookeeperRegistry({
+      logger,
+      address: '127.0.0.1:2181',
+    });
+    registry2.register({
+      interfaceName,
+      url: 'bolt://127.0.0.3:12200',
+    });
+
+    await registry.await('connected');
+    await sleep(1000);
+
+    registry.subscribe({
+      interfaceName,
+    }, val => {
+      registry.emit('second', val);
+    });
+
+    val = await registry.await('second');
+    assert.deepEqual(val, [ 'bolt://127.0.0.2:12200', 'bolt://127.0.0.3:12200' ]);
+    assert.deepEqual(firstVal, [ 'bolt://127.0.0.2:12200', 'bolt://127.0.0.3:12200' ]);
+
+    registry.close();
+    registry2.close();
+  });
+});
